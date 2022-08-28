86de1fdc6a0e3841f75fb999469d12067f814522
Islam Abdalla <islam.a.elamin@gmail.com>
2022-08-28 12:26:05 +0200

    remove home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,11 +33,9 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/Narbase/kunafa-docs',
         },
         blog: {
           showReadingTime: true,
